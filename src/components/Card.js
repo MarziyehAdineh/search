7558c26f6cfce59302bcc02e32bdba1f5818e006
marziyehadineh <MarziyehAdineh1.gmail.com>
2021-09-22 19:27:45 +0330

    commit4

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ function Card({ person }) {
   return (
     <div
       style={{
+       
         margin:"2px",
         display: "flex",
         backgroundColor: "white",
@@ -12,12 +13,12 @@ function Card({ person }) {
         padding: "3px",
       }}
     >
-      <p>
+      <p >
         <h2 style={{ margin: "0 auto" }}>{person.name}</h2>
         <p>{person.email}</p>
         <p>{person.address}</p>
       </p>
-      <img src={person.image} width="100" height="100" />
+      <img src={person.image}  width="100" height="100" />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,18 +2,22 @@ import React, { useState } from "react";
 // import '../styles.css';
 import SearchList from "./SearchList";
 function NormalizeStr(str) {
-  return str.replace("آ", "ا")
-  .replace(/[\u0629]/g, 'ه')
-  .replace("ي","ی")
-  .replace("ئ","ی")
-  .toLowerCase().trim();
+  return str
+    .replace("آ", "ا")
+    .replace(/[\u0629]/g, "ه")
+    .replace("ي", "ی")
+    .replace("ئ", "ی")
+    .toLowerCase()
+    .trim();
 }
 function TopSearchComponent({ details }) {
   const [searchField, setSearchField] = useState("");
   const filteredPersons = details.filter((person) => {
-  const sp=NormalizeStr(searchField).split(' ');
-    return (
-      sp.every(a=>NormalizeStr(person.name+" "+person.email+" "+person.address).includes(a))
+    const sp = NormalizeStr(searchField).split(" ");
+    return sp.every((a) =>
+      NormalizeStr(
+        person.name + " " + person.email + " " + person.address
+      ).includes(a)
     );
   });
 
@@ -24,23 +28,50 @@ function TopSearchComponent({ details }) {
   function searchList() {
     return (
       // <Scroll>
-        <SearchList filteredPersons={filteredPersons} />
+      <SearchList filteredPersons={filteredPersons} />
       // {/* </Scroll> */}
     );
   }
 
   return (
-    <section className="garamond" style={{margin:'0 auto', width:'400px'}}>
-      <div style={{fontFamily:"IranSans,sans-serif",textAlign:'center', border:'solid 2px black', background:'rgb(119,136,153)' }}>
-        <div className="navy georgia ma0 grow">
-          <h2 className="f2" style={{ color: "white" }}>
-           :اطلاعات خود را جستجو کنید
+    <section style={{ margin: "0 auto", width: "400px" }}>
+      <div
+        style={{
+          fontFamily: "IranSans,sans-serif",
+          textAlign: "center",
+          border: "solid 2px black",
+          background: "rgb(119,136,153)",
+        }}
+      >
+        <div>
+          <h2
+            className="f2"
+            style={{
+              color: "rgb(211,211,211)",
+              textShadow: "2px 2px 3px black",
+              fontSize: "18px",
+            }}
+          >
+            لیست اساتید دانشگاه
           </h2>
         </div>
         <div className="pa2">
-          <input 
-            style={{fontFamily:"IranSans",direction:"rtl",width:"250px"}}
-            className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+       
+          <input
+            style={{
+              borderRadius:"2px",
+              fontFamily: "IranSans",
+              direction: "rtl",
+              fontSize: "16px",
+              width: "300px",
+              height:"20px",
+              box: "border-box",
+              border: "1px solid #ddd",
+              backgroundColor: "#eee",
+              color: "#333",
+              padding: "15px"
+              
+            }}
             type="search"
             placeholder="جستجو"
             onChange={handleChange}
